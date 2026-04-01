48954110315c5a1a789d36d143e2d1135ac0e590
Minoru Furusawa <frsw3nr@gmail.com>
2026-04-02 04:14:45 +0900

    fix: compile script

--- PtuneSync/scripts/compile.ps1
+++ PtuneSync/scripts/compile.ps1
@@ -3,7 +3,7 @@ param(
     [string]$Platform = "x64"
 )
 
-Write-Host "== PtuneSync Build (MSIX) =="
+Write-Host "== PtuneSync Build (MSIX Release) =="
 
 $vsMsbuild = "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
 
@@ -19,12 +19,18 @@ Get-ChildItem "$PSScriptRoot\..\obj\" -Filter *.manifest -Recurse |
 Remove-Item -Force -ErrorAction SilentlyContinue
 
 Write-Host "== Build start =="
+
 & $vsMsbuild "$PSScriptRoot\..\PtuneSync.csproj" `
-    /t:"Restore;Build" `
+    /t:"Restore;Rebuild" `
     /p:Configuration=$Config `
     /p:Platform=$Platform `
     /p:GenerateAppxPackageOnBuild=true `
-    /p:AppxPackageValidationEnabled=false `
+    /p:AppxBundle=Always `
+    /p:AppxBundlePlatforms=$Platform `
+    /p:UapAppxPackageBuildMode=StoreUpload `
+    /p:AppxPackageDir="$PSScriptRoot\..\AppPackages\" `
+    /p:AppxPackageSigningEnabled=true `
+    /p:AppxPackageValidationEnabled=true `
     /p:BuildingFromScript=true
 
 if ($LASTEXITCODE -ne 0) {
